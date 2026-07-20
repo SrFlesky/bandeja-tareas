1ae97f980c3a3985b5e6e0fd83bd136cea97d1e3
Flesky <juandmr722@gmail.com>
2026-07-20 13:57:13 -0500

    refactor(filterConfig): remove assignedTo filter from filterConfig

--- src/features/tasks/config/filterConfig.js
+++ src/features/tasks/config/filterConfig.js
@@ -7,5 +7,5 @@ export const filterConfig = [
   { field: "status", label: "Estado", options: TASK_STATUS_LIST },
   { field: "priority", label: "Prioridad", options: TASK_PRIORITY_LIST },
   { field: "process", label: "Proceso", options: TASK_PROCESS_LIST },
-  { field: "assignedTo", label: "Responsable", options: mockEmployees.map(employees => employees.name)},
+  //{ field: "assignedTo", label: "Responsable", options: mockEmployees.map(employees => employees.name)},
 ];
