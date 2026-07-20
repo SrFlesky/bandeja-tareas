bdd24ec7acef24a20b04857229f52b00d1e0ee11
Flesky <juandmr722@gmail.com>
2026-07-20 13:44:57 -0500

    fix(filterConfig): assignedTo filter now uses only the name of the employee, not an object

--- src/features/tasks/config/filterConfig.js
+++ src/features/tasks/config/filterConfig.js
@@ -3,11 +3,9 @@ import { TASK_PRIORITY_LIST } from "../../../constants/taskPriority";
 import { TASK_PROCESS_LIST } from "../../../constants/taskProcess";
 import mockEmployees from "../../../data/mockEmployees.json";
 
-let EMPLOYEES_LIST = [...mockEmployees];
-
 export const filterConfig = [
   { field: "status", label: "Estado", options: TASK_STATUS_LIST },
   { field: "priority", label: "Prioridad", options: TASK_PRIORITY_LIST },
   { field: "process", label: "Proceso", options: TASK_PROCESS_LIST },
-  { field: "assignedTo", label: "Responsable", options: EMPLOYEES_LIST },
+  { field: "assignedTo", label: "Responsable", options: mockEmployees.map(employees => employees.name)},
 ];
