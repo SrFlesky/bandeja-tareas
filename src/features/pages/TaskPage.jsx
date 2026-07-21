04d44989d846d5e02d6b92a5778ef3602ba64612
Flesky <juandmr722@gmail.com>
2026-07-21 11:57:36 -0500

    style(TaskPage): add a div to the Search, Filter and  Sort button, so I can position them in the same line

--- src/features/pages/TaskPage.jsx
+++ src/features/pages/TaskPage.jsx
@@ -1,15 +1,15 @@
 import { useTasks } from "../tasks/hooks/useTasks";
 import { useTaskSearch } from "../tasks/hooks/useTaskSearch";
-import { useTaskFilter } from '../tasks/hooks/useTaskFilter';
+import { useTaskFilter } from "../tasks/hooks/useTaskFilter";
 import { useTaskSort } from "../tasks/hooks/useTaskSort";
-import { useSelectedTask } from '../tasks/hooks/useSelectedTask';
+import { useSelectedTask } from "../tasks/hooks/useSelectedTask";
 import SearchBar from "../tasks/components/SearchBar/SearchBar";
-import FilterBar from '../tasks/components/FilterBar/FilterBar';
+import FilterBar from "../tasks/components/FilterBar/FilterBar";
 import SortButton from "../tasks/components/SortButton/SortButton";
-import TaskDetail from '../tasks/components/TaskDetail/TaskDetail';
+import TaskDetail from "../tasks/components/TaskDetail/TaskDetail";
 import TaskCard from "../tasks/components/TaskCard/TaskCard";
 import { searchTasks } from "../tasks/utils/searchTasks";
-import { filterTasks } from '../tasks/utils/filterTasks';
+import { filterTasks } from "../tasks/utils/filterTasks";
 import { sortTasks } from "../tasks/utils/sortTasks";
 import formatDate from "../tasks/utils/formatDate";
 
@@ -25,24 +25,28 @@ function TasksPage() {
 
   const searchedTasks = searchTasks(tasks, inputValue);
   const filteredTasks = filterTasks(searchedTasks, filters);
-  const sortedTasks = sortTasks(filteredTasks, sortOrder); 
+  const sortedTasks = sortTasks(filteredTasks, sortOrder);
 
   return (
     <div className="px-4 py-4 md:px-8 md:py-6 max-w-4xl md:mx-auto">
-      <h1 className="text-xl font-bold mb-4">Bandeja de tareas</h1>
 
-      <SearchBar value={inputValue} onChange={setInputValue} />
-      <FilterBar filters={filters} toggleFilter={toggleFilter} />
-      <SortButton sortOrder={sortOrder} toggleSort={toggleSort} />
+      <div className="flex w-full gap-2">
+        <SearchBar value={inputValue} onChange={setInputValue} />
+        <FilterBar filters={filters} toggleFilter={toggleFilter} />
+        <SortButton sortOrder={sortOrder} toggleSort={toggleSort} />
+      </div>
 
       <ul className="flex flex-col gap-2 mt-4">
         {sortedTasks.map((task) => (
-          <TaskCard key={task.id} task={task} onClick={() => openTask(task.id)} />
+          <TaskCard
+            key={task.id}
+            task={task}
+            onClick={() => openTask(task.id)}
+          />
         ))}
       </ul>
 
       <TaskDetail task={selectedTask} onClose={closeTask} />
-
     </div>
   );
 }
