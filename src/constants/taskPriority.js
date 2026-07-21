236d506da07950c4037e51ac22cfdce6737266e6
Flesky <juandmr722@gmail.com>
2026-07-21 15:43:37 -0500

    feature(constants): add new Key by label to the status and priority task

--- src/constants/taskPriority.js
+++ src/constants/taskPriority.js
@@ -5,3 +5,7 @@ export const TASK_PRIORITY = {
 };
 
 export const TASK_PRIORITY_LIST = Object.values(TASK_PRIORITY);
+
+export const PRIORITY_KEY_BY_LABEL = Object.fromEntries(
+  Object.entries(TASK_PRIORITY).map(([key, label]) => [label, key])
+);
--- src/constants/taskStatus.js
+++ src/constants/taskStatus.js
@@ -1,7 +1,11 @@
 export const TASK_STATUS = {
   PENDING: 'Pendiente',
-  IN_PROGRESS: 'En Progreso',
+  PROGRESS: 'En Progreso',
   FINISHED: 'Completada',
 };
 
 export const TASK_STATUS_LIST = Object.values(TASK_STATUS);
+
+export const STATUS_KEY_BY_LABEL = Object.fromEntries(
+  Object.entries(TASK_STATUS).map(([key, label]) => [label, key])
+);
