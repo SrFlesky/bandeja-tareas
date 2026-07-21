078c8a49d4410228ecc5a7148bf3fca53a0ae9cf
Flesky <juandmr722@gmail.com>
2026-07-21 10:27:41 -0500

    feature: add key by label array, so when getting the process values in spanish the server don't need to re render

--- src/constants/taskProcess.js
+++ src/constants/taskProcess.js
@@ -1,14 +1,18 @@
 export const TASK_PROCESS = {
-  ACCOUNTING: 'Contabilidad',
-  COLLECTIONS: 'Cartera',
-  MARKETING: 'Marketing',
-  TI: 'TI',
-  LOGISTICS: 'Logística',
-  TREASURY: 'Tesorería',
-  AUDIT: 'Auditoría',
-  INFRASTRUCTURE: 'Infraestructura',
-  LEGAL: 'Legal',
-  OTHER: 'Others'
+  ACCOUNTING: "Contabilidad",
+  COLLECTIONS: "Cartera",
+  MARKETING: "Marketing",
+  TI: "TI",
+  LOGISTICS: "Logística",
+  TREASURY: "Tesorería",
+  AUDIT: "Auditoría",
+  INFRASTRUCTURE: "Infraestructura",
+  LEGAL: "Legal",
+  OTHER: "Others",
 };
 
 export const TASK_PROCESS_LIST = Object.values(TASK_PROCESS);
+
+export const PROCESS_KEY_BY_LABEL = Object.fromEntries(
+  Object.entries(TASK_PROCESS).map(([key, label]) => [label, key])
+);
