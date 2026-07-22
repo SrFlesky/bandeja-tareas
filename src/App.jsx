7341595ff3eab3c824fe6463e6d39421e2c72930
Flesky <juandmr722@gmail.com>
2026-07-22 10:25:14 -0500

    feature(App): incuded Bottom Nav and Top Bar to the main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import TasksPage from "./features/pages/TasksPage";
+import TopBar from "./shared/components/TopBar/TopBar";
 import NavBar from "./shared/components/NavBar/NavBar";
+import TasksPage from "./features/pages/TasksPage";
+import BottomNav from "./shared/components/BottomNav/BottomNav";
 
 function App() {
   return (
     <div className="flex h-screen overflow-hidden">
       <NavBar activePage="tasks" />
-      <div className="flex-1 min-w-0 overflow-y-auto">
-        <TasksPage />
+
+      <div className="flex flex-col flex-1 overflow-hidden">
+        <TopBar />
+        <div className="flex-1 min-w-0 overflow-y-auto bg-page-bg">
+          <TasksPage />
+        </div>
+        <BottomNav activePage="tasks" />
       </div>
     </div>
   );
